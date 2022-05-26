c18054bfc840169466077a8386bfda55ba17c38d
Shawn Witemyre <switemyre@comcast.net>
2022-05-25 23:00:57 -0400

    Created stats and item-list components. Added both to the views module.

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-views-home',
   templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  styleUrls: ['./views-home.component.css'],
 })
 export class ViewsHomeComponent implements OnInit {
+  stats: any = [
+    { value: 22, label: '# of Users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Satsifaction Score' },
+  ];
 
-  constructor() { }
+  items: any = [
+    {
+      images: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a couch',
+    },
+    {
+      images: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a dresser',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
